e1c9b2c7151f81ebc4714c5b6159575ed208f1f8
Jorge Balarezo <127998906+Jbloca@users.noreply.github.com>
2023-09-02 11:11:41 -0500

    Update ejercicio2.java

--- ejercicio2.java
+++ ejercicio2.java
@@ -1,18 +1,19 @@
 
-import java.util.scanner;
+import java.util.Scanner;
 
 public class Main
 {
 	public static void main(String[] args) {
 		int numero;
 		
-		scanner leer = new Scanner(System.in);
+		Scanner leer = new Scanner(System.in);
 		
-		System.out.System.out.println("Ingrese un numero");
+		System.out.println("Ingrese un numero");
 		numero = leer.nextInt();
 		
 		if(numero > 0 && numero%2 !=0) {
-		    System.out.println("true");   
+		    System.out.println("true");
+		    
 		}
 
 	    
